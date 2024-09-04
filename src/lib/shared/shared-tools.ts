592250588b5ab17ef90b7e358a576282a65b1688
Kyle Truong <ktruong008@gmail.com>
2024-09-04 12:41:13 -0400

    refactor out auxillary fns into shared file

--- src/lib/shared/shared-tools.ts
+++ src/lib/shared/shared-tools.ts
@@ -1,3 +1,8 @@
+import { fabric } from 'fabric';
+
+import Walrus from '$lib/assets/walrus-250.png';
+import Penguin from '$lib/assets/penguin-150.png';
+
 import { OBJECT_IDS, CANVAS_BG_HEIGHT, CANVAS_BG_WIDTH } from './shared.constant';
 
 export const getObjectId = (key: string) => {
@@ -12,4 +17,169 @@ export function calculateRelativePosition(x, y, imgWidth, imgHeight) {
     left: (x / CANVAS_BG_WIDTH) * imgWidth,
     top: (y / CANVAS_BG_HEIGHT) * imgHeight
   };
+}
+
+export const updateCanvasSize = (fabricCanvas, containerDiv) => {
+  fabricCanvas.setWidth(containerDiv.clientWidth);
+  fabricCanvas.setHeight(containerDiv.clientHeight);
+  fabricCanvas.renderAll();
+};
+
+/**
+ * Event handlers
+ */
+export function handleMouseDown(opt, fabricCanvas) {
+  const evt = opt.e;
+  startDragging(evt.clientX, evt.clientY, fabricCanvas);
+}
+
+export function handleMouseMove(opt, fabricCanvas, imgWidth, imgHeight) {
+  if (fabricCanvas.isDragging) {
+    const evt = opt.e;
+    moveCanvas(evt.clientX, evt.clientY, fabricCanvas, imgWidth, imgHeight);
+  }
+}
+
+export function handleMouseUp(fabricCanvas) {
+  fabricCanvas.isDragging = false;
+}
+
+export function startDragging(x, y, fabricCanvas) {
+  fabricCanvas.isDragging = true;
+  fabricCanvas.lastPosX = x;
+  fabricCanvas.lastPosY = y;
+}
+
+export function moveCanvas(x, y, fabricCanvas, imgWidth, imgHeight) {
+  const vpt = fabricCanvas.viewportTransform;
+
+  // Calculate the new position
+  let newX = vpt[4] + x - fabricCanvas.lastPosX;
+  let newY = vpt[5] + y - fabricCanvas.lastPosY;
+
+  // Limit the dragging within the image boundaries
+  const maxX = 0;
+  const minX = fabricCanvas.width - imgWidth;
+  const maxY = 0;
+  const minY = fabricCanvas.height - imgHeight;
+
+  newX = Math.min(Math.max(newX, minX), maxX);
+  newY = Math.min(Math.max(newY, minY), maxY);
+
+  // Update the viewport transform
+  vpt[4] = newX;
+  vpt[5] = newY;
+
+  fabricCanvas.requestRenderAll();
+  fabricCanvas.lastPosX = x;
+  fabricCanvas.lastPosY = y;
+
+  fabricCanvas.forEachObject((obj) => {
+    obj.setCoords();
+  });
+}
+
+/**
+ * Interactive areas
+ */
+export function addInteractiveAreas(imgWidth, imgHeight, fabricCanvas) {
+  /**
+   * Walrus
+   */
+  const walrusPosition = calculateRelativePosition(100, 200, imgWidth, imgHeight);
+
+  fabric.Image.fromURL(Walrus, (img) => {
+    // Set image properties
+    img.set({
+      left: walrusPosition.left,
+      top: walrusPosition.top,
+      scaleX: (0.5 * imgWidth) / CANVAS_BG_WIDTH,
+      scaleY: (0.5 * imgHeight) / CANVAS_BG_HEIGHT,
+      selectable: false,
+      evented: true,
+      hoverCursor: 'pointer'
+    });
+
+    const group = new fabric.Group([img], {
+      selectable: false,
+      evented: true,
+      hoverCursor: 'pointer'
+    }) as any;
+
+    // Add hover effect
+    group.on('mousedown', () => {
+      group.animate('top', group?.top - 10, {
+        duration: 200,
+        onChange: fabricCanvas.renderAll.bind(fabricCanvas),
+        easing: fabric.util.ease.easeOutCubic
+      });
+    });
+
+    group.on('mouseup', () => {
+      group.animate('top', group?.top + 10, {
+        duration: 200,
+        onChange: fabricCanvas.renderAll.bind(fabricCanvas),
+        easing: fabric.util.ease.easeOutCubic
+      });
+    });
+
+    group.on('mouseup', function () {
+      console.log('up');
+    });
+
+    // Add the image to the canvas
+    fabricCanvas.add(group);
+    group.bringToFront();
+    fabricCanvas.renderAll();
+  });
+
+  /**
+   * Penguin
+   */
+  const penguinPosition = calculateRelativePosition(100, 100, imgWidth, imgHeight);
+
+  fabric.Image.fromURL(Penguin, (img) => {
+    // Set image properties
+    img.set({
+      left: penguinPosition.left,
+      top: penguinPosition.top,
+      scaleX: (0.5 * imgWidth) / CANVAS_BG_WIDTH,
+      scaleY: (0.5 * imgHeight) / CANVAS_BG_HEIGHT,
+      selectable: false,
+      evented: true,
+      hoverCursor: 'pointer'
+    });
+
+    const group = new fabric.Group([img], {
+      selectable: false,
+      evented: true,
+      hoverCursor: 'pointer'
+    }) as any;
+
+    // Add hover effect
+    group.on('mousedown', () => {
+      group.animate('top', group?.top - 10, {
+        duration: 200,
+        onChange: fabricCanvas.renderAll.bind(fabricCanvas),
+        easing: fabric.util.ease.easeOutCubic
+      });
+    });
+
+    group.on('mouseup', () => {
+      group.animate('top', group?.top + 10, {
+        duration: 200,
+        onChange: fabricCanvas.renderAll.bind(fabricCanvas),
+        easing: fabric.util.ease.easeOutCubic
+      });
+    });
+
+    group.on('mouseup', function () {
+      console.log('up');
+    });
+
+    // Add the image to the canvas
+    fabricCanvas.add(group);
+    group.bringToFront();
+    fabricCanvas.renderAll();
+  });
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,12 +11,15 @@
   import { Transaction } from '@mysten/sui/transactions';
 
   import { Button } from '$lib/components/ui/button';
-  import { calculateRelativePosition, getObjectId } from '$lib/shared/shared-tools';
+  import {
+    addInteractiveAreas,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    updateCanvasSize
+  } from '$lib/shared/shared-tools';
   import { fish, mintWalrus } from '$lib/sdk/sdk';
-  import Penguin from '$lib/assets/penguin-150.png';
-  import Walrus from '$lib/assets/walrus-250.png';
   import Ice from '$lib/assets/ice-1080x720.png';
-  import { CANVAS_BG_HEIGHT, CANVAS_BG_WIDTH } from '$lib/shared/shared.constant';
 
   const walletAdapter =
     PUBLIC_NODE_ENV === 'production' ? productionWalletAdapter : testnetWalletAdapter;
@@ -28,14 +31,11 @@
   let fabricCanvas;
   let containerDiv;
 
+  /**
+   * Mount canvas
+   */
   onMount(() => {
     let imgWidth, imgHeight;
-
-    const updateCanvasSize = () => {
-      fabricCanvas.setWidth(containerDiv.clientWidth);
-      fabricCanvas.setHeight(containerDiv.clientHeight);
-      fabricCanvas.renderAll();
-    };
 
     fabricCanvas = new fabric.Canvas(canvas, {
       width: window.innerWidth,
@@ -73,185 +73,31 @@
         (fabricCanvas.height - img.height * scaleFactor) / 2;
 
       fabricCanvas.renderAll();
-      addInteractiveAreas(imgWidth, imgHeight);
+      addInteractiveAreas(imgWidth, imgHeight, fabricCanvas);
     });
 
     /**
      * Event listeners
      */
     // Enable panning
-    fabricCanvas.on('mouse:down', handleMouseDown);
-    fabricCanvas.on('mouse:move', handleMouseMove);
-    fabricCanvas.on('mouse:up', handleMouseUp);
-
-    function handleMouseDown(opt) {
-      const evt = opt.e;
-      startDragging(evt.clientX, evt.clientY);
-    }
-
-    function handleMouseMove(opt) {
-      if (fabricCanvas.isDragging) {
-        const evt = opt.e;
-        moveCanvas(evt.clientX, evt.clientY);
-      }
-    }
-
-    function handleMouseUp() {
-      fabricCanvas.isDragging = false;
-    }
-
-    function startDragging(x, y) {
-      fabricCanvas.isDragging = true;
-      fabricCanvas.lastPosX = x;
-      fabricCanvas.lastPosY = y;
-    }
-
-    function moveCanvas(x, y) {
-      const vpt = fabricCanvas.viewportTransform;
-
-      // Calculate the new position
-      let newX = vpt[4] + x - fabricCanvas.lastPosX;
-      let newY = vpt[5] + y - fabricCanvas.lastPosY;
-
-      // Limit the dragging within the image boundaries
-      const maxX = 0;
-      const minX = fabricCanvas.width - imgWidth;
-      const maxY = 0;
-      const minY = fabricCanvas.height - imgHeight;
-
-      newX = Math.min(Math.max(newX, minX), maxX);
-      newY = Math.min(Math.max(newY, minY), maxY);
-
-      // Update the viewport transform
-      vpt[4] = newX;
-      vpt[5] = newY;
-
-      fabricCanvas.requestRenderAll();
-      fabricCanvas.lastPosX = x;
-      fabricCanvas.lastPosY = y;
-
-      fabricCanvas.forEachObject((obj) => {
-        obj.setCoords();
-      });
-    }
+    fabricCanvas.on('mouse:down', (opt) => handleMouseDown(opt, fabricCanvas));
+    fabricCanvas.on('mouse:move', (opt) =>
+      handleMouseMove(opt, fabricCanvas, imgWidth, imgHeight)
+    );
+    fabricCanvas.on('mouse:up', () => handleMouseUp(fabricCanvas));
 
     // Resize canvas when window is resized
-    window.addEventListener('resize', updateCanvasSize);
+    window.addEventListener('resize', () =>
+      updateCanvasSize(fabricCanvas, containerDiv)
+    );
 
     return () => {
-      window.removeEventListener('resize', updateCanvasSize);
+      window.removeEventListener('resize', () =>
+        updateCanvasSize(fabricCanvas, containerDiv)
+      );
     };
   });
-
-  /**
-   * Interactive areas
-   */
-  function addInteractiveAreas(imgWidth, imgHeight) {
-    /**
-     * Walrus
-     */
-    const walrusPosition = calculateRelativePosition(100, 200, imgWidth, imgHeight);
-
-    fabric.Image.fromURL(Walrus, (img) => {
-      // Set image properties
-      img.set({
-        left: walrusPosition.left,
-        top: walrusPosition.top,
-        scaleX: (0.5 * imgWidth) / CANVAS_BG_WIDTH,
-        scaleY: (0.5 * imgHeight) / CANVAS_BG_HEIGHT,
-        selectable: false,
-        evented: true,
-        hoverCursor: 'pointer'
-      });
-
-      const group = new fabric.Group([img], {
-        selectable: false,
-        evented: true,
-        hoverCursor: 'pointer'
-      }) as any;
-
-      // Add hover effect
-      group.on('mousedown', () => {
-        group.animate('top', group?.top - 10, {
-          duration: 200,
-          onChange: fabricCanvas.renderAll.bind(fabricCanvas),
-          easing: fabric.util.ease.easeOutCubic
-        });
-      });
-
-      group.on('mouseup', () => {
-        group.animate('top', group?.top + 10, {
-          duration: 200,
-          onChange: fabricCanvas.renderAll.bind(fabricCanvas),
-          easing: fabric.util.ease.easeOutCubic
-        });
-      });
-
-      group.on('mouseup', function () {
-        console.log('up');
-      });
-
-      // Add the image to the canvas
-      fabricCanvas.add(group);
-      group.bringToFront();
-      fabricCanvas.renderAll();
-    });
-
-    /**
-     * Penguin
-     */
-    const penguinPosition = calculateRelativePosition(100, 100, imgWidth, imgHeight);
-
-    fabric.Image.fromURL(Penguin, (img) => {
-      // Set image properties
-      img.set({
-        left: penguinPosition.left,
-        top: penguinPosition.top,
-        scaleX: (0.5 * imgWidth) / CANVAS_BG_WIDTH,
-        scaleY: (0.5 * imgHeight) / CANVAS_BG_HEIGHT,
-        selectable: false,
-        evented: true,
-        hoverCursor: 'pointer'
-      });
-
-      const group = new fabric.Group([img], {
-        selectable: false,
-        evented: true,
-        hoverCursor: 'pointer'
-      }) as any;
-
-      // Add hover effect
-      group.on('mousedown', () => {
-        group.animate('top', group?.top - 10, {
-          duration: 200,
-          onChange: fabricCanvas.renderAll.bind(fabricCanvas),
-          easing: fabric.util.ease.easeOutCubic
-        });
-      });
-
-      group.on('mouseup', () => {
-        group.animate('top', group?.top + 10, {
-          duration: 200,
-          onChange: fabricCanvas.renderAll.bind(fabricCanvas),
-          easing: fabric.util.ease.easeOutCubic
-        });
-      });
-
-      group.on('mouseup', function () {
-        console.log('up');
-      });
-
-      // Add the image to the canvas
-      fabricCanvas.add(group);
-      group.bringToFront();
-      fabricCanvas.renderAll();
-    });
-  }
 </script>
-
-<div bind:this={containerDiv} class="canvas-container relative">
-  <canvas bind:this={canvas}></canvas>
-</div>
 
 <ConnectButton {walletAdapter} />
 
@@ -259,6 +105,10 @@
 <Button onclick={() => fish(walrusObjectId)}>Fish</Button>
 <Button>Claim fish</Button>
 <Button>Add penguin</Button>
+
+<div bind:this={containerDiv} class="canvas-container relative">
+  <canvas bind:this={canvas}></canvas>
+</div>
 
 <style>
   .canvas-container {
